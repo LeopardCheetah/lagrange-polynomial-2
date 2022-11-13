fc596287cf106ac636b9113e9a8363c9c73bfdf8
LeopardCheetah <hawkswithmath@gmail.com>
2022-11-12 17:29:48 -0800

    rewrote base + some basic point getter made

--- polynomialmaker.py
+++ polynomialmaker.py
@@ -1,32 +1,45 @@
-"""
-import sympy
+import sys # exits code
 
-x, y = symbols('x y')
+print("this program will hopefully find a line that passes through N points")
+print("only integer coordinates please :)")
+# todo: accept fractions
+point_count = input("How many points will you input?\n")
 
-exp = sympy.expand((X+Y)**2)
-  
-print(exp)
-"""
-import sympy
+try:
+    point_count = int(point_count)
+    print()
+except:
+    print("That is not an integer number of points!")
+    sys.exit(1)
+    # program is terminated
 
-print('simple polynomial finder, more reliable than matrices (at least more reliable than last time)')
+if point_count < 0:
+    print("You cannot input a negative amount of points")
+    sys.exit(1)
 
-#number of points the p goes through 
-pointnum = int(input('how many points will you enter?'))
 
-xcoordinates = []
-ycoordinates = []
+x_coords = []
+y_coords = []
+for i in range(point_count):
+    print(f"Enter the x and y coordinates of the {i+1}th number please (seperated by a space): ")
+    x, y = input().split()
+    x_coords.append(int(x))
+    y_coords.append(int(y))
+    # check that it is a legtimiate function (1 does not map to 2 and 3)
 
-for i in range(pointnum):
-    x, y = input('give me the x and y coordinates of number ', (i+1)).split()
-    xcoordinates.append(int(x))
-    ycoordinates.append(int(y))
-    
-#the nth point's coordinates are listed in (xcoordinates[n-1], ycoordinates[n-1])
+    if len(x_coords) != len(set(x_coords)):
+        #not a function, overlap
+        print("the point that was entered makes an equation impossible (not a function)")
+        sys.exit(1)
 
-a = symbols('a')
-poly = ''
+print(x_coords, y_coords)
 
-for j in xcoordinates:
-    topstr = ''
-    topstr
+# some base cases
+
+if point_count == 0:
+    print("y = 0")
+    sys.exit(0)
+
+if point_count == 1:
+    print(f"y = {y_coords[0]}")
+    sys.exit(0)
